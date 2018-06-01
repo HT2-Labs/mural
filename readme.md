85c539acaab095b247ab15975e6ecfce67a065bc
Ryan Smith <0ryansmith1994@gmail.com>
2018-06-01 19:10:08 +0100

    feat: Starts package.

---
+++ readme.md
@@ -0,0 +1,6 @@
+# React Components
+> A package of React components for use within HT2 Labs.
+
+```sh
+npm i @ht2-labs/react-components
+```
---
+++ scripts/ghpages.js
@@ -0,0 +1,17 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish('dist', {
+  user: {
+    name: 'HT2 Bot',
+    email: 'hello@ht2labs.com'
+  },
+  repo: `https://${process.env.GH_TOKEN}@github.com/js-entity-repos/todos.git`,
+  silent: true,
+  message: 'ci(gh-pages): Publishes new changes [ci skip].'
+}, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Published to GH Pages');
+});
