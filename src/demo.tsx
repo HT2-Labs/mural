85bc8f36812db5abdbb1dfa911f63d91049bd4b1
Callum Williams <callum.williams@ht2labs.com>
2018-06-23 22:24:16 +0100

    Add icons to the docs

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,17 +1,68 @@
 import * as React from 'react';
 import * as dom from 'react-dom';
+import styled from 'styled-components';
 import ButtonIcon, { Size } from './buttons/ButtonIcon';
+
+import IconArrowDown from './icons/IconArrowDown';
+import IconArrowLeft from './icons/IconArrowLeft';
+import IconArrowRight from './icons/IconArrowRight';
+import IconArrowUp from './icons/IconArrowUp';
+import IconAsterisk from './icons/IconAsterisk';
+import IconBullseye from './icons/IconBullseye';
+import IconCalendar from './icons/IconCalendar';
+import IconEllipsisV from './icons/IconEllipsisV';
+import IconFilter from './icons/IconFilter';
+import IconFlagCheckered from './icons/IconFlagCheckered';
 import IconHome from './icons/IconHome';
+import IconLink from './icons/IconLink';
+import IconPlusCircle from './icons/IconPlusCircle';
 import IconSearch from './icons/IconSearch';
+import IconThumbsUp from './icons/IconThumbsUp';
+import IconTimes from './icons/IconTimes';
 
 const Demo = () => {
+  const Grid = styled.div`
+    display: grid;
+    grid-template-columns: 200px 200px 200px 200px 200px;
+    grid-template-rows: auto;
+    grid-column-gap: 10px;
+    grid-row-gap: 10px;
+  `;
+  const GridItem = styled.div`
+    border: 1px solid #eee;
+    display: flex;
+    align-items: center;
+    font-size: 3em;
+    flex-direction: column;
+    padding: 20px 4px;
+  `;
+  const GridItemText = styled.div`
+    padding-top: 20px;
+    font-size: 16px;
+  `;
   return (
     <>
-      <div>This is our home icon: <IconHome /></div>
-      <div>This is our search icon: <IconSearch /></div>
+      <Grid>
+        <GridItem><IconArrowDown /><GridItemText>IconArrowDown</GridItemText></GridItem>
+        <GridItem><IconArrowLeft /><GridItemText>IconArrowLeft</GridItemText></GridItem>
+        <GridItem><IconArrowRight /><GridItemText>IconArrowRight</GridItemText></GridItem>
+        <GridItem><IconArrowUp /><GridItemText>IconArrowUp</GridItemText></GridItem>
+        <GridItem><IconAsterisk /><GridItemText>IconAsterisk</GridItemText></GridItem>
+        <GridItem><IconBullseye /><GridItemText>IconBullseye</GridItemText></GridItem>
+        <GridItem><IconCalendar /><GridItemText>IconCalendar</GridItemText></GridItem>
+        <GridItem><IconEllipsisV /><GridItemText>IconEllipsisV</GridItemText></GridItem>
+        <GridItem><IconFilter /><GridItemText>IconFilter</GridItemText></GridItem>
+        <GridItem><IconFlagCheckered /><GridItemText>IconFlagCheckered</GridItemText></GridItem>
+        <GridItem><IconHome /><GridItemText>IconHome</GridItemText></GridItem>
+        <GridItem><IconLink /><GridItemText>IconLink</GridItemText></GridItem>
+        <GridItem><IconPlusCircle /><GridItemText>IconPlusCircle</GridItemText></GridItem>
+        <GridItem><IconSearch /><GridItemText>IconSearch</GridItemText></GridItem>
+        <GridItem><IconThumbsUp /><GridItemText>IconThumbsUp</GridItemText></GridItem>
+        <GridItem><IconTimes /><GridItemText>IconTimes</GridItemText></GridItem>
+      </Grid>
       <div>
         This is our button icon:
-        <ButtonIcon renderIcon={IconHome} size={Size.Large} backgroundColor="red" color="white" />
+        <ButtonIcon renderIcon={IconHome} size={Size.Medium} backgroundColor="red" color="white" />
       </div>
     </>
   );
