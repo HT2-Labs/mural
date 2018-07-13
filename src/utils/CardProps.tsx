a70c448f3636956d520f339c4cbb23a68a6b6775
Callum Williams <callum.williams@ht2labs.com>
2018-07-13 10:30:05 +0100

    fix(Props): Add React.MouseEvent to onClick

--- src/utils/CardProps.tsx
+++ src/utils/CardProps.tsx
@@ -6,6 +6,6 @@ export enum CardType {
 export default interface IconProps {
   readonly active?: boolean;
   readonly children?: JSX.Element[] | JSX.Element | string | boolean[] | boolean;
-  readonly onClick?: (e: Event) => void;
+  readonly onClick?: (e: React.MouseEvent) => void;
   readonly type?: CardType;
 }
--- src/utils/MenuProps.tsx
+++ src/utils/MenuProps.tsx
@@ -1,5 +1,5 @@
 export default interface IconProps {
   readonly active?: boolean;
   readonly children?: JSX.Element[] | JSX.Element | string;
-  readonly onClick?: (e: Event) => void;
+  readonly onClick?: (e: React.MouseEvent) => void;
 }
