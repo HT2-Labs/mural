4ff3e54bc19a645cb1d27414db9b8e02603461ec
Callum Williams <callum.williams@ht2labs.com>
2018-07-26 14:32:54 +0100

    fix(Text): Move export file

---
+++ src/Text.ts
@@ -0,0 +1,14 @@
+import H1 from './text/H1';
+import H2 from './text/H2';
+import H3 from './text/H3';
+import H4 from './text/H4';
+import P from './text/P';
+import { SizeType } from './text/TextProps';
+export {
+  H1,
+  H2,
+  H3,
+  H4,
+  P,
+  SizeType,
+};
