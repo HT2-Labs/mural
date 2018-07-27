96f357d02e8f217005009813d7bd02aa41b15c3a
Callum Williams <callum.williams@ht2labs.com>
2018-07-27 19:22:24 +0100

    docs(Modal): Add example code

--- src/docs/DocsModal.tsx
+++ src/docs/DocsModal.tsx
@@ -1,9 +1,10 @@
 // tslint:disable:no-this
 // tslint:disable:prefer-function-over-method
 // tslint:disable:no-class
+// tslint:disable:max-file-line-count
 import * as React from 'react';
 
-import { H1, H3, P, SizeType } from '../Text';
+import { H1, H2, H3, P, SizeType } from '../Text';
 
 import { Button, ButtonGroup, ButtonType } from '../Button';
 import { IconClose } from '../Icon';
@@ -12,10 +13,43 @@ import ModalActions from '../modal/ModalActions';
 import ModalBody from '../modal/ModalBody';
 import ModalFooter from '../modal/ModalFooter';
 import ModalHeader from '../modal/ModalHeader';
+import DocsCode from './DocsCode';
+
+const codeSnippet =
+`<Modal open={this.state.modalOpen} onCloseFn={this.closeModal}>
+<ModalHeader>
+  <H3 size={SizeType.Large}>This is a Modal</H3>
+  <ModalActions>
+    <Button type={ButtonType.Invisible} onClick={this.closeModal}>
+      <IconClose />
+    </Button>
+  </ModalActions>
+</ModalHeader>
+
+<ModalBody>
+  <P>Oh what a cool thing!</P>
+  <P>Oh what a cool thing!</P>
+  <P>Oh what a cool thing!</P>
+  <P>Oh what a cool thing!</P>
+  <P>Oh what a cool thing!</P>
+  <P>Oh what a cool thing!</P>
+</ModalBody>
+
+<ModalFooter>
+  <P>Oh what a cool thing!</P>
+  <ModalActions>
+    <ButtonGroup>
+      <Button>Yes</Button>
+      <Button type={ButtonType.Ghost}>Hmm</Button>
+      <Button onClick={this.closeModal} danger>No!</Button>
+    </ButtonGroup>
+  </ModalActions>
+</ModalFooter>
+</Modal>`;
 
 export default class DocsModal extends React.Component {
   public readonly state = {
-    modalOpen: true,
+    modalOpen: false,
   };
 
   public readonly openModal = () => {
@@ -66,6 +100,10 @@ export default class DocsModal extends React.Component {
             </ModalActions>
           </ModalFooter>
         </Modal>
+
+        <H2 size={SizeType.Large}>Examples</H2>
+        <hr />
+        <DocsCode code={codeSnippet} />
       </>
     );
   }
