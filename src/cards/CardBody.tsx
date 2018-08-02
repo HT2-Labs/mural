70834105fb70d4054c88f91dc14b502f129ec3d9
Callum Williams <callum.williams@ht2labs.com>
2018-08-02 16:16:46 +0100

    feat(Card): Add overlay action

--- src/cards/CardBody.tsx
+++ src/cards/CardBody.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import CardProps from './CardProps';
 
-export default ({ children }: CardProps) => {
+export interface CardBodyProps {
+  readonly children?: React.ReactNode;
+  readonly onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+}
+
+export default ({ children, onClick }: CardBodyProps) => {
   const CardBody = styled.div`
     display: flex;
     font-size: 16px;
     height: 100%;
     line-height: 1.19;
     width: 100%;
+    &:hover {
+      ${onClick !== undefined ? 'cursor: pointer' : ''};
+      ${onClick !== undefined ? 'text-decoration: underline' : ''};
+    }
   `;
   return (
-    <CardBody>
+    <CardBody onClick={onClick}>
       {children}
     </CardBody>
   );
