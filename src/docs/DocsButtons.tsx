6423a750777278fc033f9257e517796555195d2b
Kakuridoji <41896047+Kakuridoji@users.noreply.github.com>
2018-08-07 14:00:31 +0100

    docs(DoAndDont's): added dos and don'ts component to docs (#58)

--- src/docs/DocsButtons.tsx
+++ src/docs/DocsButtons.tsx
@@ -29,6 +29,7 @@ import IconBullseye from '../icons/IconBullseye';
 import IconCalendar from '../icons/IconCalendar';
 import IconFlagCheckered from '../icons/IconFlagCheckered';
 import IconTimesCircle from '../icons/IconTimesCircle';
+import DocsDoDont from './DocsDoDont';
 
 const codeSnippetButton =
   `import {
@@ -257,6 +258,14 @@ export default () => {
             <Button>6</Button>
           </ButtonGroup>
         </DocsPlayground>
+      </DocsDescription>
+      <DocsDescription title="Dos And Don'ts">
+      <DocsDoDont>
+        <TableRow><TableCell> Use button group if using more than one button</TableCell><TableCell> Don't use invisible buttons on their own</TableCell></TableRow>
+        <TableRow><TableCell> Use invisible buttons along with normal buttons</TableCell><TableCell> Don't use icon buttons for dangerous actions</TableCell></TableRow>
+        <TableRow><TableCell> Use danger buttons to indicate dangerous actions</TableCell><TableCell> Don't use normal buttons for dangerous actions</TableCell></TableRow>
+        <TableRow><TableCell> Use ghost buttons to overlay splash images </TableCell><TableCell> </TableCell></TableRow>
+      </DocsDoDont>
       </DocsDescription>
     </>
   );
--- src/tables/Table.tsx
+++ src/tables/Table.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
 import styled from '../../node_modules/styled-components';
 
+export enum Layout {
+  auto,
+  fixed,
+}
+
 interface TabMenuProps {
     readonly children: React.ReactNode;
+    readonly layout?: string;
   }
 
-  export default ({ children }: TabMenuProps) => {
+  export default ({ children, layout }: TabMenuProps) => {
     const Table = styled.table`
       width: 100%;
       border-collapse: collapse;
       background-color: #eaeaea;
       padding: 4px;
+      ${layout !== undefined ? `table-layout: ${layout};` : null}
     `;
 
     return (
