93da1ed279986507765c152c8331bd9b9f21e677
Callum Williams <callum.williams@ht2labs.com>
2018-08-22 17:36:16 +0100

    feat(Grid): Add Grid component

---
+++ src/layouts/Grid.ts
@@ -0,0 +1,21 @@
+import styled from '../utils/mural-styled-components';
+import { stripUnit } from 'polished';
+
+const Grid = styled.div<{
+  readonly columns?: number;
+  readonly rowHeight?: string;
+}>`
+  display: grid;
+  grid-column-gap: 20px;
+  grid-row-gap: 20px;
+  ${(props) => props.columns !== undefined
+    ? `grid-template-columns: repeat(${props.columns}, ${props.columns}fr)`
+    : 'grid-template-columns: repeat(1, 1fr)'
+  };
+  ${(props) => props.rowHeight !== undefined
+    ? `grid-template-rows: repeat(auto-fill, ${props.rowHeight})`
+    : `grid-template-rows: repeat(auto-fill, 1fr)`
+  };
+`;
+
+export default Grid;
