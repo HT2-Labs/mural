bff70c162bedeb446dba14df9a2123c40a9568a7
Callum Williams <callum.williams@ht2labs.com>
2018-08-28 19:54:53 +0100

    feat(Card): Add Card Media component

--- src/cards/index.ts
+++ src/cards/index.ts
@@ -2,10 +2,12 @@ import Card from './Card';
 import CardBody from './CardBody';
 import CardFooter from './CardFooter';
 import CardHeroText from './CardHeroText';
+import CardMedia from './CardMedia';
 
 export {
   Card,
   CardBody,
   CardFooter,
   CardHeroText,
+  CardMedia,
 };
