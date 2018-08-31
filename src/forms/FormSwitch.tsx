c1bd936bdb4f9d333ae2c634bd1a9a2fa9c98d70
Pete Thorne <ee0pdt@users.noreply.github.com>
2018-08-31 11:09:07 +0100

    fix(FormSwitch): Sets color to same as all other buttons
    * fix(FormSwitch): Sets color to same as all other buttons
    
    * Moves Formswitch elements outside the component

--- src/forms/FormSwitch.tsx
+++ src/forms/FormSwitch.tsx
@@ -3,49 +3,49 @@ import styled from 'styled-components';
 import buttonColorStyles from '../buttons/utils/buttonColorStyles';
 import FormProps from './formProps';
 
+const SwitchBody = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+`;
+
+const Slider = styled.div`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: 34px;
+  &:before {
+    border-radius: 50%;
+    position: absolute;
+    content: '';
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+`;
+
 const FormSwitch = (props: FormProps) => {
   const disabled = props.disabled;
-  const color = buttonColorStyles({disabled});
-
-  const SwitchBody = styled.label`
-    position: relative;
-    display: inline-block;
-    width: 60px;
-    height: 34px;
-  `;
-
-  const Slider = styled.div`
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: .4s;
-    transition: .4s;
-    border-radius: 34px;
-    &:before {
-      border-radius: 50%;
-      position: absolute;
-      content: "";
-      height: 26px;
-      width: 26px;
-      left: 4px;
-      bottom: 4px;
-      background-color: white;
-      -webkit-transition: .4s;
-      transition: .4s;
-    }
-  `;
+  const color = buttonColorStyles({ disabled });
 
   const CheckBox = styled.input`
     z-index: 1000;
     &:checked + ${Slider} {
-      background-color: ${color.backgroundLight};
+      background-color: ${color.background};
     }
     &:focus + ${Slider} {
-      box-shadow: 0 0 1px #2196F3;
+      box-shadow: 0 0 1px #2196f3;
     }
     &:checked + ${Slider}:before {
       transform: translateX(26px);
@@ -54,8 +54,8 @@ const FormSwitch = (props: FormProps) => {
 
   return (
     <SwitchBody>
-      <CheckBox type="checkbox" {...props}/>
-      <Slider/>
+      <CheckBox type="checkbox" {...props} />
+      <Slider />
     </SwitchBody>
   );
 };
