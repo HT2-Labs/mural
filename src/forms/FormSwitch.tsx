571165acf6c5c1920e0e3115c6bb0d5eae245359
Pete Thorne <pete.thorne@ht2.co.uk>
2018-08-31 11:04:05 +0100

    Moves Formswitch elements outside the component

--- src/forms/FormSwitch.tsx
+++ src/forms/FormSwitch.tsx
@@ -3,41 +3,41 @@ import styled from 'styled-components';
 import buttonColorStyles from '../buttons/utils/buttonColorStyles';
 import FormProps from './formProps';
 
+const SwitchBody = styled.label`
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+`;
+
+const Slider = styled.div`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: 34px;
+  &:before {
+    border-radius: 50%;
+    position: absolute;
+    content: '';
+    height: 26px;
+    width: 26px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+`;
+
 const FormSwitch = (props: FormProps) => {
   const disabled = props.disabled;
   const color = buttonColorStyles({ disabled });
-
-  const SwitchBody = styled.label`
-    position: relative;
-    display: inline-block;
-    width: 60px;
-    height: 34px;
-  `;
-
-  const Slider = styled.div`
-    position: absolute;
-    cursor: pointer;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background-color: #ccc;
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
-    border-radius: 34px;
-    &:before {
-      border-radius: 50%;
-      position: absolute;
-      content: '';
-      height: 26px;
-      width: 26px;
-      left: 4px;
-      bottom: 4px;
-      background-color: white;
-      -webkit-transition: 0.4s;
-      transition: 0.4s;
-    }
-  `;
 
   const CheckBox = styled.input`
     z-index: 1000;
