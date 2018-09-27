7c310cd21a170d9d2e58ac92884c5a7d438838c9
Callum Williams <callum.williams@ht2labs.com>
2018-09-27 14:42:12 +0100

    Add exports and update docs

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -5,6 +5,7 @@ import IconArrowUp from './IconArrowUp';
 import IconAsterisk from './IconAsterisk';
 import IconBullseye from './IconBullseye';
 import IconCalendar from './IconCalendar';
+import IconChartBar from './IconChartBar';
 import IconCheck from './IconCheck';
 import IconCheckCircle from './IconCheckCircle';
 import IconChevronDown from './IconChevronDown';
@@ -13,6 +14,7 @@ import IconChevronRight from './IconChevronRight';
 import IconChevronUp from './IconChevronUp';
 import IconClose from './IconClose';
 import IconCode from './IconCode';
+import IconCog from './IconCog';
 import IconCuratr from './IconCuratr';
 import IconEdit from './IconEdit';
 import IconEllipsisV from './IconEllipsisV';
@@ -22,12 +24,18 @@ import IconFilter from './IconFilter';
 import IconFlagCheckered from './IconFlagCheckered';
 import IconGithub from './IconGithub';
 import IconHome from './IconHome';
+import IconInfoCircle from './IconInfoCircle';
 import IconLink from './IconLink';
 import IconNpm from './IconNpm';
+import IconPen from './IconPen';
 import IconPlusCircle from './IconPlusCircle';
 import IconProject from './IconProject';
 import IconSearch from './IconSearch';
+import IconStarHalf from './IconStarHalf';
+import IconStarRegular from './IconStarRegular';
+import IconStarSolid from './IconStarSolid';
 import IconTachometer from './IconTachometer';
+import IconThumbsDown from './IconThumbsDown';
 import IconThumbsUp from './IconThumbsUp';
 import IconTimes from './IconTimes';
 import IconTimesCircle from './IconTimesCircle';
@@ -40,6 +48,7 @@ export {
   IconAsterisk,
   IconBullseye,
   IconCalendar,
+  IconChartBar,
   IconCheck,
   IconCheckCircle,
   IconChevronDown,
@@ -48,6 +57,7 @@ export {
   IconChevronUp,
   IconClose,
   IconCode,
+  IconCog,
   IconCuratr,
   IconEdit,
   IconEllipsisV,
@@ -57,12 +67,18 @@ export {
   IconFlagCheckered,
   IconGithub,
   IconHome,
+  IconInfoCircle,
   IconLink,
   IconNpm,
+  IconPen,
   IconPlusCircle,
   IconProject,
   IconSearch,
+  IconStarHalf,
+  IconStarRegular,
+  IconStarSolid,
   IconTachometer,
+  IconThumbsDown,
   IconThumbsUp,
   IconTimes,
   IconTimesCircle,
