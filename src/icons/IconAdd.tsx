e8c5cfa617071b56e49c3777e305e80b46c10b4d
Sophia <sophia.markoglou@ht2labs.com>
2018-10-17 15:27:50 +0100

    Rename IconAdd with IconPlus

--- src/icons/IconAdd.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import createSVGIcon from '../utils/createSVGIcon';
-
-export default createSVGIcon(() => {
-  return (
-    <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/>
-  );
-}, '0 0 24 24' );
