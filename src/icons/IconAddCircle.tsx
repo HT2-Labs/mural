effc51770806c91412aa67b903095abf8a8a1ab3
Sophia <sophia.markoglou@ht2labs.com>
2018-10-17 15:12:27 +0100

    feat(Icon): Add AddCircle Icon

---
+++ src/icons/IconAddCircle.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import createSVGIcon from '../utils/createSVGIcon';
+
+export default createSVGIcon(() => {
+  return (
+    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" />
+  );
+}, '0 0 24 24');
